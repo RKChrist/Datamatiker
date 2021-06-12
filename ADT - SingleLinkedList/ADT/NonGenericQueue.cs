a460307b1f98404a60d9b0369a6b18c0045d30e4
RKChrist <76455111+RKChrist@users.noreply.github.com>
2021-06-12 18:11:16 +0200

    SingleLinkedList
    This is how i created a queue, stack and single linked list

---
+++ ADT - SingleLinkedList/ADT/NonGenericQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    public class NonGenericQueue
+    {
+        NonGenericLinkedList queue = new NonGenericLinkedList();
+
+        public int Count { get; private set; } = 0;
+
+        public bool IsEmpty { get; set; } = true;
+
+        public void Enqueue(object obj)
+        {
+            queue.Append(obj);
+            IsEmpty = false;
+            Count++;
+        }
+
+        public object Dequeue()
+        {
+            var obj = queue.ItemAt(0);
+            queue.DeleteAt(0);
+            Count--;
+            if (Count == 0)
+            {
+                IsEmpty = true;
+            }
+            return (obj);
+        }
+    }
+}
+
---
+++ ADT - SingleLinkedList/TestProject1/QueueUnitTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADT;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class QueueUnitTest
+    {
+        [TestMethod]
+        public void TestQueue()
+        {
+            // Standard queue implemented by .NET
+            System.Collections.Generic.Queue<int> stdQueue = new System.Collections.Generic.Queue<int>();
+            // Your manually implemented ADT queue
+            Queue<int> yourQueue = new Queue<int>();
+
+            // Test generates 100 random integers and adds to both queues
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int randVal = r.Next();
+                stdQueue.Enqueue(randVal);
+                yourQueue.Enqueue(randVal);
+            }
+
+            // Test reading back half the added integers
+            for (int i = 0; i < 50; i++)
+            {
+                Assert.AreEqual(stdQueue.Dequeue(), yourQueue.Dequeue());
+                Assert.AreEqual(stdQueue.Count, yourQueue.Count);
+            }
+
+            // Test adding 50 more random integers
+            for (int i = 0; i < 50; i++)
+            {
+                int randVal = r.Next();
+                stdQueue.Enqueue(randVal);
+                yourQueue.Enqueue(randVal);
+            }
+
+            // Test reading back all the remaining values
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.AreEqual(stdQueue.Dequeue(), yourQueue.Dequeue());
+                Assert.AreEqual(stdQueue.Count, yourQueue.Count);
+            }
+
+            // Test queue is empty now
+            Assert.AreEqual(true, yourQueue.IsEmpty);
+        }
+    }
+}
