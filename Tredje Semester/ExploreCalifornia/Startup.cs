24b33a6d5c2c88314c6537d08cec4c1169304470
RKChrist <roo12ney@gmail.com>
2021-08-25 20:21:09 +0200

    The Basis
    The Basis is done

---
+++ Tredje Semester/ExploreCalifornia/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                DeveloperExtensions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env,
+            FeatureToggles feature
+            )
+        {
+            
+            if (feature.DeveloperExtensions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error.html");
+                app.UseHsts();
+            }
+
+
+            ////creating error (The basics (Error handling and diagnostics)
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                {
+                    throw new ArgumentException("ERROR");
+                }
+                await next();
+                
+            });
+
+            app.UseFileServer();
+
+
+            //First Exercise
+            //OLD 
+            //Use is to aynsc use that lamda expression before app.run
+            // Hvis du vil bruge next.Invoke(), så kommer den efter din run method, selvom din run method terminator din pipeline.
+                    //app.Use(async (context, next) =>
+                    //{
+                    //    //if (context.Request.Path.Value.StartsWith("/Hello"))
+                    //    //{
+                    //    //    await context.Response.WriteAsync("Bitch");
+                    //    //}
+                    //    await next.Invoke();
+
+                    //    await context.Response.WriteAsync("Hello from 1nd Delegate");
+                    //});
+          
+                    //app.Run(async (context) =>
+                    //{
+                    //    await context.Response.WriteAsync("Hello from 2nd Delegate");
+                    //});
+
+
+        }
+    }
+}
